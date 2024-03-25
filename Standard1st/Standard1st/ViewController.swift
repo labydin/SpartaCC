d27c7cbd04029b6f1f8873b2181ebb8ef25d0690
Junyeong <labydin@naver.com>
2024-03-25 20:53:07 +0900

    스탠다드반 첫주차 예습

---
+++ Standard1st/Standard1st/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Standard1st
+//
+//  Created by 박준영 on 3/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+    }
+
+
+}
+
